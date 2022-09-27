d49afebd639e685d4a6e8028284ee5e4112898dc
Melanie Roberts <robertsmelaniecc@gmail.com>
2022-09-27 18:25:55 -0400

    first

---
+++ script.js
@@ -0,0 +1,35 @@
+//  1
+// const
+
+//  2
+let num = 20
+console.log(num);
+//  3
+num = 15
+console.log(num);
+//  4
+num = num + 7
+console.log(num);
+//  5
+num = num - 1 
+console.log(num);
+// 6
+num = 'This is not a number';
+console.log(num);
+//  7
+let blank;
+
+//  8
+console.log(blank);
+
+//  9
+blank = null;
+console.log(blank);
+//  Bonus 1
+const quarter = 0.25, dime = 0.10, nickel = 0.05, penny = 0.01;
+//  Bonus 2
+console.log(quarter, dime, nickel, penny);
+//  Bonus 3
+let letters = 'qwertyuiop'
+//  Bonus 4
+console.log(letters[letters.length -1]);
