523fce469a23430452f3c0d7a41dd971aca58741
Melanie Roberts <robertsmelaniecc@gmail.com>
2022-09-28 10:04:54 -0400

    After class review

--- script.js
+++ script.js
@@ -2,16 +2,19 @@
 // const
 
 //  2
-let num = 20
+let num = 20;
 console.log(num);
 //  3
-num = 15
+num = 15;
 console.log(num);
 //  4
-num = num + 7
+num = num + 7;
+// also num += 7;
 console.log(num);
 //  5
-num = num - 1 
+num --;
+//incorrect needed to do it without an equals sign num -= 1;
+//see above num = num - 1; 
 console.log(num);
 // 6
 num = 'This is not a number';
@@ -27,9 +30,13 @@ blank = null;
 console.log(blank);
 //  Bonus 1
 const quarter = 0.25, dime = 0.10, nickel = 0.05, penny = 0.01;
+// other way const [quarter, dime, nickel, penny] = [0.25, 0.10, 0.05, 0.01];
+
 //  Bonus 2
 console.log(quarter, dime, nickel, penny);
 //  Bonus 3
-let letters = 'qwertyuiop'
+let letters = 'qwertyuiop';
 //  Bonus 4
 console.log(letters[letters.length -1]);
+//also console.log(letters.length)
+// also console.log(letters[9]);
